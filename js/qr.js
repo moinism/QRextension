2885d7766a52de22a0b7b42680055b04a71286f0
Moin Uddin <me@moin.im>
2013-12-13 20:05:28 +0500

    v0.5.1 release
    Made some changes to show QR as images, instead of canvas. And also to
    show that image's 64Base code data.

--- js/qr.js
+++ js/qr.js
@@ -5,4 +5,11 @@ if(window.location.hash) {
 $(function() {
 	$("#data").text(data);
 	$("#qrcode").qrcode(data);
+	var qr = $('canvas');
+	// qr.getContext("2d"); 
+    var qrData = qr[0].toDataURL("image/png");
+    $('#img').attr('src',qrData);
+    $('#qrData').html(qrData).click(function() {
+    	$(this).select();
+    });
 });
