75411bc75ba0e066379adc9b8ca826435f7a7ecc
James Healy <James-Healy@users.noreply.github.com>
2014-06-24 17:39:17 +0100

    Convert to Event Page

--- js/background.js
+++ js/background.js
@@ -1,19 +1,36 @@
 function qr (data) {
 	chrome.tabs.create({url:"qr.html#"+data});
 }
-function qrLink (info,tab) {
-	qr(info.linkUrl);
-}
-function qrPage (info,tab) {
-	qr(info.pageUrl);
-}
-function qrImg (info,tab) {
-	qr(info.srcUrl);
-}
-function qrText (info,tab) {
-	qr(info.selectionText);
-}
-chrome.contextMenus.create({"title": "QR this link", "contexts":["link"], "onclick": qrLink});
-chrome.contextMenus.create({"title": "QR this image", "contexts":["image"], "onclick": qrImg});
-chrome.contextMenus.create({"title": "QR this page", "contexts":["page"], "onclick": qrPage});
-chrome.contextMenus.create({"title": "QR '%s'", "contexts":["selection"], "onclick": qrText});
+
+
+function onClickHandler(info, tab) {
+	switch(info.menuItemId) {
+		case "qrLink":
+			qr(info.linkUrl);
+			break;
+		
+		case "qrImg":
+			qr(info.srcUrl);
+			break;
+		
+		case "qrPage":
+			qr(info.pageUrl);
+			break;
+		
+		case "qrText":
+			qr(info.selectionText);
+			break;
+	}
+};
+
+
+chrome.contextMenus.onClicked.addListener(onClickHandler);
+
+
+chrome.runtime.onInstalled.addListener(function (){
+	chrome.contextMenus.create({"title": "QR this link", "contexts":["link"], "id": "qrLink"});
+	chrome.contextMenus.create({"title": "QR this image", "contexts":["image"], "id": "qrImg"});
+	chrome.contextMenus.create({"title": "QR this page", "contexts":["page"], "id": "qrPage"});
+	chrome.contextMenus.create({"title": "QR '%s'", "contexts":["selection"], "id": "qrText"});
+});
+
