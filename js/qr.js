35843e56b4eaf1aebbc48d3377ef7ff865267888
Simon <simonschllng@users.noreply.github.com>
2022-11-22 12:44:34 +0000

    decode URI to work with text selection

--- js/qr.js
+++ js/qr.js
@@ -1,6 +1,6 @@
 var data = 'http://moin.im';
 if(window.location.hash) {
-	data = window.location.hash.replace('#','');
+	data = decodeURI(window.location.hash.replace('#',''));
 }
 $(function() {
 	$("#data").text(data);
